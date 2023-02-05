f792cb4ae2b70b151545890b239be179342c9f32
RyanGormican <ryan.gormican@gmail.clom>
2023-02-05 03:26:55 -0600

    a

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -5,8 +5,17 @@ import {useEffect, useState) from "react";
 export default function Dashbord(){
 	const route = useRouter();
 	const [user, loading] = useAuthState(auth);
+
+	const getData = async () => { 
+
 	if (loading) retrun; 
 	if(!user) return route.push("/auth/login");
+
+	};
+
+	useEffect(() => {
+	getData() 
+	}, [user,loading]);
 	return(
 		<div>
 			<h1> Your posts</h1>
