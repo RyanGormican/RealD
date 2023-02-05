f7e603507a12ab57940b7f197b3c5e2cf26a02b2
RyanGormican <ryan.gormican@gmail.clom>
2023-02-05 03:09:42 -0600

    a

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,16 +1,31 @@
 import { Icon } from '@iconify/react';
 import {signInWithPopup, GoogleAuthProvider} from "firebase/auth";
 import { auth } from "../../utils/firebase";
+import {useRouter} from 'next/router';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useEffect} from 'react';
 export default function Login(){
+	const route = useRouter();
+	const [user, loading] = useAuthState(auth);
 		//Google 
 		const googleProvider = new GoogleAuthProvider();
 		const GoogleLogin = async () => {
 			try {
 				const result = await signInWithPopup(auth, googleProvider);
+				route.push("/");
 			} catch (error) {
-			console.log("test");
+			console.log(error);
 			};
 		}
+		useEffect(() => {
+			if(user){
+				route.push("/");
+			}
+			else
+			{
+				console.log("login")
+			}
+		} , [user]);
 	return(
 		<div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
 			<h2 className="text-2xl font-medium">Join Today</h2>
