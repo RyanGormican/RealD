d7268c575c0f2bc746dd852b5e999d65d2e67753
RyanGormican <ryan.gormican@gmail.clom>
2023-02-05 03:16:45 -0600

    a

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import { Icon } from '@iconify/react';
+import {auth} from "../utils/firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
 export default function Nav() {
+const [user, loading]  = useAuthState(auth);
 	return (
 	<nav className="flex justify-between items-center py-10">
 		<Link href="/">
@@ -15,9 +18,21 @@ export default function Nav() {
 			</a>
 		</div>
 		<ul className="flex items-center gap-10">
+		{!user && (
 		<a href={"/auth/login"} className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
 			Join Now! 
 		</a> 
+		)}
+		{user && (
+			<div className="flex items-center gap-6">
+			<Link href="/post">
+				<button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-mg textx-small">Post </button>
+			</Link> 
+			<Link href="/dashbord">
+				<h1>Dashboard</h1>
+			</Link> 
+			</div>
+		)}
 		</ul>
 	</nav>
 	);
