7453a617ff230f8d64ebee143fb87453213ddaa4
RyanGormican <ryan.gormican@gmail.clom>
2023-02-05 18:56:47 -0600

    a
    a

--- pages/post.js
+++ pages/post.js
@@ -18,9 +18,9 @@ export default function Post(){
 					<h3 className="text-lg font-medium py-2"> Description </h3>
 					<textarea 
 						value={post.description} 
-						onChange={(e) => setPost(e.target.value)}
+						onChange={(e) => setPost({...post, description: e.target.value})}
 						className="bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-sm"></textarea>
-					<p>0/300</p>
+					<p>{post.description.length}/300</p>
 				</div> 
 				<button 
 					type="submit"
