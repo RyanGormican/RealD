aa32ebb4c9eb3dcb0e52f9ec44a687e402a48970
RyanGormican <ryan.gormican@gmail.com>
2023-02-05 22:54:18 -0600

    a
    a

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,7 +1,18 @@
+import Message from  "../components/message";
+import {auth, db} from '../utils/firebase';
+import {useRouter} from 'next/router';
+import {useEffect, useState} from 'react';
+import {toast} from 'react-toastify';
 export default function Details() {
+	const router = useRouter();
+	const routeData = router.query;
+	const [message,setMessage] = useState('');
+	const [allMessage,setAllMessages] = useState([]);
 	return(
 		<div>
-			<h1> Hi </h1>
+			<Message {...routeData}>
+
+			</Message> 
 		</div>
 	);
 }
