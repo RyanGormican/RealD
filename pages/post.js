10d69ccee6e884db4814bd412814b97693de65f1
RyanGormican <ryan.gormican@gmail.clom>
2023-02-05 18:52:56 -0600

    a

--- pages/post.js
+++ pages/post.js
@@ -4,16 +4,29 @@ import {useAuthState} from 'react-firebase-hooks/auth';
 import {useEffect, useState} from 'react';
 
 export default function Post(){
+	// state
+	const [post, setPost] = useState({description : ""});
+	// submit post 
+	const submitPost = async (e) => {
+		e.preventDefault();
+	};
 	return (
 		<div className="my-20 p-12 shadow-lg rounded-lg max-w-md mx-auto">
-			<form>
+			<form onSubmit={submitPost}>
 				<h1 className="text-2xl font-bold"> Create a new post </h1>
 				<div className="py-2">
 					<h3 className="text-lg font-medium py-2"> Description </h3>
-					<textarea className="bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-sm"></textarea>
+					<textarea 
+						value={post.description} 
+						onChange={(e) => setPost(e.target.value)}
+						className="bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-sm"></textarea>
 					<p>0/300</p>
 				</div> 
-				<button className="w-full bg-cyan-600 text-white font-medium p-2 my-2 rounded-lg text-sm">Submit</button>
+				<button 
+					type="submit"
+					className="w-full bg-cyan-600 text-white font-medium p-2 my-2 rounded-lg text-sm">
+						Submit
+				</button>
 			</form>
 		</div>
 	);
