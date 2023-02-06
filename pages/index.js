f941a73641154867c316e0ea951b62aa26684fc5
RyanGormican <ryan.gormican@gmail.com>
2023-02-05 23:53:29 -0600

    a
    a

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
             {allPosts.map((post) => (
             <Message key={post.id}{...post}>
                 <Link href={{pathname: `/${post.id}`, query:{...post}}}>
-                <button> comments </button>
+                <button>{post.comments?.length > 0 ? post.comments?.length : 0} comments </button>
                 </Link>
             </Message>
             ))}
